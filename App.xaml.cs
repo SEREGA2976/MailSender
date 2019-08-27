01ebfa9146a57d2d7ef3223efcf71890fea04765
Sergey <47604847+SEREGA2976@users.noreply.github.com>
2019-08-28 01:05:27 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App 
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+    }
+}
